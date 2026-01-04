f5350db65f477f2f541a6184c8ddbfa356c1a94d
Matthias-Ricard <matthias.ricard@hotmail.fr>
2026-01-04 11:52:32 +0100

    test 3

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,25 +1,35 @@
+let allWorks = [];
+
+
+
 fetch("http://localhost:5678/api/works")
   .then(response => response.json())
   .then(works => {
-    const gallery = document.querySelector(".gallery");
-
-    works.forEach(work => {
-        const figure = document.createElement("figure");
-
-        const img = document.createElement("img");
-        img.src = work.imageUrl;
-        img.alt = work.title;
-
-        const figcaption = document.createElement("figcaption");
-        figcaption.textContent = work.title;
-
-        figure.appendChild(img);
-        figure.appendChild(figcaption);
-        gallery.appendChild(figure);
-
-    });
+    allWorks = works;
+    displayWorks(works);
 
   });
+
+  function displayWorks(works) {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = ""; // on vide la galerie
+
+  works.forEach(work => {
+    const figure = document.createElement("figure");
+
+    const img = document.createElement("img");
+    img.src = work.imageUrl;
+    img.alt = work.title;
+
+    const figcaption = document.createElement("figcaption");
+    figcaption.textContent = work.title;
+
+    figure.appendChild(img);
+    figure.appendChild(figcaption);
+    gallery.appendChild(figure);
+  });
+}
+
 
 const filtersContainer = document.querySelector(".filters");
 
@@ -30,13 +40,38 @@ fetch("http://localhost:5678/api/categories")
     // bouton Tous
     const allButton = document.createElement("button");
     allButton.textContent = "Tous";
-    filtersContainer.appendChild(allButton);
+
+    allButton.addEventListener("click", () => {
+    displayWorks(allWorks);
+    setActiveButton(allButton);
+    });
+
+filtersContainer.appendChild(allButton);
+
 
     // boutons catégories
-    categories.forEach(category => {
-      const button = document.createElement("button");
-      button.textContent = category.name;
-      filtersContainer.appendChild(button);
-    });
-  });
+  categories.forEach(category => {
+    const button = document.createElement("button");
+    button.textContent = category.name;
 
+    button.addEventListener("click", () => {
+      const filteredWorks = allWorks.filter(
+        work => work.categoryId === category.id
+      );
+      displayWorks(filteredWorks);
+      setActiveButton(button);
+    });
+
+    filtersContainer.appendChild(button);
+    });
+
+    
+});
+
+function setActiveButton(clickedButton) {
+  const buttons = document.querySelectorAll(".filters button");
+  buttons.forEach(button => {
+    button.classList.remove("active");
+  });
+  clickedButton.classList.add("active");
+}
