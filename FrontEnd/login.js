d58aea39d737eaebc459fb5989c4f539323fe950
Matthias-Ricard <matthias.ricard@hotmail.fr>
2026-01-15 10:35:09 +0100

    test

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,11 +1,11 @@
-const form = document.querySelector("#login-form")
-const errorMessage = document.querySelector(".error-message")
+const form = document.querySelector("form")
+
 
 form.addEventListener("submit", async (event) =>{
     event.preventDefault();
 
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
     let user = {
         email: email,
         password: password
@@ -13,21 +13,32 @@ form.addEventListener("submit", async (event) =>{
 
     console.log(email, password)
 
-    try {
-        const response = await fetch("http://localhost:5678/api/users/login", {
-            method: "POST",
-            heasers: {"Content-Type": "application/json"},
-            body: JSON.stringify({user})
-        });
-        if (!response.ok) {
-            throw new Error("Erreur dans l'identifiant ou le mot de passe");
+    
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        heasers: {"Content-Type": "application/json"},
+        body: JSON.stringify({user})
+    })
+
+    .then(response => response.json())
+
+    .then(data => {
+        if(data.token) {
+            localStorage.setItem("Token", data.token)
+            window.location.href = "index.html"
         }
-        const data = await response.json();
-        localStorage.setItem("token", data.token);
+        else {
+            let loginErreur = document.querySelector("#login-form")
+            let mdp = document.querySelector(".forgot-password")
+            let pErreur = document.querySelector(".error-message")
 
-        window.location.href = "index.html";
-    } catch (error) {
-        errorMessage.textContent = "Erreur dans l'identifiant ou le mot de passe";
-    }
+            if (!pErreur) {
+                pErreur = document.createElement("p")
+                pErreur.classList.add("error-message")
+                loginErreur.insertBefore(pErreur, mdp)
+            }
+
+            pErreur.innerHTML = "Erreur dans l’identifiant ou le mot de passe !"
+        }
+    })
 })
-
